671e88b2844f536b6e8946ae57582bd447fa5560
William <will880123@gmail.com>
2025-07-30 19:47:03 +0800

    chore: Configure Vite dev server on port 8080 and allow external access

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,         // 修改啟動的 port
+    host: true,         // 讓外部設備可以連進來
+    strictPort: true    // 如果 port 被佔用就報錯（非必須）
+  },
 })
